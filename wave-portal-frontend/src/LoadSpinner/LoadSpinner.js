fc6c52606cb980292717588819da2ab9567aa3b2
santiagotroa <santiago.roa@mercadolibre.com>
2021-12-28 15:49:10 -0300

    Removed unused spinner

--- wave-portal-frontend/src/LoadSpinner/LoadSpinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './LoadSpinner.css';
-
-const LoadSpinner = () => (
-  <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-);
-
-export default LoadSpinner;
